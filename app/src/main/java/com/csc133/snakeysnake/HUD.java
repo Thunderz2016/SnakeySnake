5eec19105db8493b35e28ca64d7da3c0d9f4896b
Thunderz2016 <24950670+Thunderz2016@users.noreply.github.com>
2024-03-27 03:32:52 -0700

    Refactor SnakeGame draw method

--- app/src/main/java/com/csc133/snakeysnake/HUD.java
+++ app/src/main/java/com/csc133/snakeysnake/HUD.java
@@ -61,19 +61,19 @@ public class HUD extends Drawable {
         mPaint.setTextAlign(Paint.Align.LEFT);   // Reset alignment for all other text
     }
 
-    void setFont(Context context, Paint mPaint) {
+    void setFont(Context context) {
         Typeface nes = Typeface.createFromAsset(context.getAssets(), "nes.otf");
         mPaint.setTypeface(nes);
     }
 
-    void drawBackgroundBitmap(Context context, Canvas c, Paint p) {
+    void drawBackgroundBitmap(Context context) {
         // Create a background bitmap
         Bitmap mBitmapBackground = BitmapFactory
                 .decodeResource(context.getResources(),
                         R.drawable.background);
 
         // Draw the background bitmap
-        c.drawBitmap(mBitmapBackground, -1920, -1350, p);
+        mCanvas.drawBitmap(mBitmapBackground, -1920, -1350, mPaint);
     }
 
     void drawScore(Canvas mCanvas, Paint mPaint, int score) {
